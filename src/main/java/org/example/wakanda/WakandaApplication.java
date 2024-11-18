e42ac478d704cf120f2ee80e7ebed0aa7c53f5cc
mbp4 <mblanpon@myuax.com>
2024-11-18 09:13:35 +0100

    Initial commit

---
+++ src/main/java/org/example/wakanda/WakandaApplication.java
@@ -0,0 +1,13 @@
+package org.example.wakanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakandaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WakandaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wakanda/WakandaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wakanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakandaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
