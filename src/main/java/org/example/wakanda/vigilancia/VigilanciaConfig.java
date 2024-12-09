6beb2cb72517670d9bda168b70ee639c8f66ea7a
mbp4 <mblanpon@myuax.com>
2024-12-09 11:23:43 +0100

    servicio para la vigilancia creado

--- src/main/java/org/example/wakanda/vigilancia/VigilanciaConfig.java
+++ src/main/java/org/example/wakanda/vigilancia/VigilanciaConfig.java
@@ -1,4 +1,17 @@
 package org.example.wakanda.vigilancia;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.*;
+
+import java.util.concurrent.*;
+
+@Configuration
+@EnableDiscoveryClient
 public class VigilanciaConfig {
+
+    @Bean
+    public ExecutorService vigilanciaExecutorService() {
+        return Executors.newFixedThreadPool(5);
+    }
+
 }
--- src/main/java/org/example/wakanda/vigilancia/VigilanciaController.java
+++ src/main/java/org/example/wakanda/vigilancia/VigilanciaController.java
@@ -1,7 +1,27 @@
 package org.example.wakanda.vigilancia;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class VigilanciaController {
+
+    private final VigilanciaService vigilanciaService;
+
+    public VigilanciaController(VigilanciaService vigilanciaService) {
+        this.vigilanciaService = vigilanciaService;
+    }
+
+    @GetMapping("/camaras")
+    public Map<String, String> getCamaraStatus() {
+        return vigilanciaService.getCamaraStatus();
+    }
+
+    @GetMapping("/drones")
+    public Map<String, String> getDronePatrolStatus() {
+        return vigilanciaService.getDronePatrolStatus();
+    }
+
 }
