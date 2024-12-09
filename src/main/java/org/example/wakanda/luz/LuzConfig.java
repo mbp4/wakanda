b5ec087fd2081352d7df6733c78b8ca0c73f0cde
mbp4 <mblanpon@myuax.com>
2024-12-09 11:15:53 +0100

    servicio para la luz creado

--- src/main/java/org/example/wakanda/luz/LuzConfig.java
+++ src/main/java/org/example/wakanda/luz/LuzConfig.java
@@ -1,4 +1,18 @@
 package org.example.wakanda.luz;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableDiscoveryClient
 public class LuzConfig {
+
+    @Bean
+    public ExecutorService luzExecutorService() {
+        return Executors.newFixedThreadPool(5);
+    }
 }
--- src/main/java/org/example/wakanda/luz/LuzService.java
+++ src/main/java/org/example/wakanda/luz/LuzService.java
@@ -2,7 +2,29 @@ package org.example.wakanda.luz;
 
 import org.jvnet.hk2.annotations.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class LuzService {
-    
+    private Map<String, String> iluminacionPublica = new HashMap<>();
+    private Map<String, String> smartGrid = new HashMap<>();
+
+    public LuzService() {
+        iluminacionPublica.put("Calle Principal", "Activa");
+        iluminacionPublica.put("Calle Secundaria", "Inactiva");
+        iluminacionPublica.put("Plaza principal", "Activa");
+        iluminacionPublica.put("Carreteras", "Activa");
+
+        smartGrid.put("Energía Solar", "100% Operativo");
+        smartGrid.put("Energía Eólica", "75% Operativo");
+    }
+
+    public Map<String, String> getIluminacionPublica() {
+        return iluminacionPublica;
+    }
+
+    public Map<String, String> getSmartGridStatus() {
+        return smartGrid;
+    }
 }
