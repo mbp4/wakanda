6ea989c3273b1cf06efc06a2e155608ac91dc7ee
mbp4 <mblanpon@myuax.com>
2024-12-09 11:20:42 +0100

    servicio para el ocio creado

--- src/main/java/org/example/wakanda/ocio/OcioController.java
+++ src/main/java/org/example/wakanda/ocio/OcioController.java
@@ -1,7 +1,27 @@
 package org.example.wakanda.ocio;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("/ocio")
 public class OcioController {
+    private final OcioService ocioService;
+
+    public OcioController(OcioService ocioService) {
+        this.ocioService = ocioService;
+    }
+
+    @GetMapping("/eventos")
+    public Map<String, String> getEventosCulturales() {
+        return ocioService.getEventosCulturales();
+    }
+
+    @GetMapping("/espacios")
+    public Map<String, String> getEspaciosCulturalesStatus() {
+        return ocioService.getEspaciosCulturalesStatus();
+    }
 }
--- src/main/java/org/example/wakanda/ocio/OcioService.java
+++ src/main/java/org/example/wakanda/ocio/OcioService.java
@@ -2,6 +2,31 @@ package org.example.wakanda.ocio;
 
 import org.jvnet.hk2.annotations.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class OcioService {
+    private Map<String, String> eventos = new HashMap<>();
+    private Map<String, String> espacios = new HashMap<>();
+
+    public OcioService() {
+        eventos.put("Festival de Música", "Activo");
+        eventos.put("Concierto de Rock", "Finalizado");
+        eventos.put("Feria de Antigüedades", "Activo");
+        eventos.put("Mercadillo Navideño", "Proximamente");
+
+        espacios.put("Museo Histórico", "Abierto");
+        espacios.put("Centro Cultural", "Cerrado por mantenimiento");
+        espacios.put("Museo Black Panther", "Cerrado por mantenimiento");
+        espacios.put("Centro de reclutamiento", "Abierto");
+    }
+
+    public Map<String, String> getEventosCulturales() {
+        return eventos;
+    }
+
+    public Map<String, String> getEspaciosCulturalesStatus() {
+        return espacios;
+    }
 }
