2b0470fb9976559b5b64d884c2bd0a836781ff84
sonia <stejerec@myuax.com>
2024-12-18 17:15:28 +0100

    Servicios realizados por completo

---
+++ src/main/java/org/example/wakanda/transporte/Ruta.java
@@ -0,0 +1,41 @@
+package org.example.wakanda.transporte;
+
+class Ruta {
+    private Long id;
+    private String origen;
+    private String destino;
+    private String horario;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+}
